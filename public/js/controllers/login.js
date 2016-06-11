3a1e8a1c6aeeaf5f5bd5259ba64e47aa8f9df07a
Miguel Costero <miguelcostero@outlook.com>
2016-06-11 17:52:59 -0400

    few changes

--- public/js/controllers/login.js
+++ public/js/controllers/login.js
@@ -1,20 +1,13 @@
 var app = angular.module("adminApp")
 
-app.controller("loginController", function ($scope, Auth) {
-  $scope.menaje = "";
-
-  $scope.datos = {
-    email: "",
-    password: ""
+app.controller("loginController", function ($scope, $cookies, $location, Auth) {
+  if (_.isEqual($cookies.get("islogged"), "true")) {
+    $location.path("/dashboard")
   }
 
   $scope.login = function () {
     if (_.isEmpty($scope.datos)) return $scope.mensaje = "Ha dejado un campo vacío"
 
     Auth.login($scope.datos)
-
-    if (!_.isEmpty($scope.datos.result)) {
-      console.log("dentro");
-    }
   }
 })
